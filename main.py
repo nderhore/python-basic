f284eb60662c6b1f7d5441352c95d2a57592e6e3
Nathan Derhore <nathan@derhore.fr>
2022-10-14 09:35:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# somme de deux valeurs
+def sum_two_values(value_one: int, value_two: int):
+    # TODO
+
+# l'utilisateur , saisie deux valeurs et on affiche la somme
+# nous pouvons varier cela avec une soustraction ou encore multiplication
+def user_write_sum():
+    # exemple de saisie utilisateur : value_one = int(input("valeur une :"))
+    # TODO
+
+
+def show_max_value(value_one: int, value_two: int):
+    # affichage de la valeur la plus elevée
+    # TODO
+
+
+def major_or_not_major():
+    #l'utilisateur saisi son age, dans un nombre entier et on lui affiche un texte selon son age
+    # si son age est à 18 ou plus : "Vous êtes majeur" , sinon, "vous êtes mineur !"
+
+#José va bientot passer son bac, il effectue en ce moment une anti-seche en python qui lui permet de lui donner
+# la longueur de l'hypothenuse
+# vous devez utiliser le théorème de Pythagore et afficher la longueur de l'hypothenuse
+def pythagore(cote_un : float, cote_deux : float):
+    #TODO
+
+
+# José, désire un programme qui permet de lui donner la mention en fonction de la note
+# Vous devrez concevoir un algorithme qui demandera une note à l'utilisateur et affichera un texte selon le bareme
+# en dessous de 8 : refusé
+# entre 8 inclus et 10 exclus : rattrapage
+# entre 10 inclus et 12 exlus : pas de mention
+# entre 12 inclus et 14 exclus : mention assez bien
+# entre 14 inclus et 16 exclus : mention bien
+# au dela de 16 inclus : très bien
+def mention_bac():
+    # TODO
+
+
+#José, après avoir eu son bac, a fait une grosse fete
+# il culpabilise car il a trop mangé et souhaite connaitre son IMC.
+# demander les le poid ainsi que la taille à l'utilisateur et affichez son IMC
+# L'imc est le poid (en kg) divisé ar la taille (en m) au carré.
+def imc():
+    # TODO
+
+
+def pair_or_impar():
+    # l'utilisateur doit saisir un nombre entier: on lui affiche s'il est pair ou impair
+    # pair si quand il est divisé par deux, le reste est de 0
+
+def premier_or_not_premier():
+    # on demande à l'utilisateur un nombre entier, on lui affiche s'il est premier ou non
+    # regle : un nombre est premier seulement s'il est divisible par 1 et lui-même , 1 exclu
+
+
+# division, attention au 0 !
+# Pour rappel, une division par 0 est interdite
+def division_two_values(numerator: int, denumerator: int):
+    # TODO
+
+
+#José est revenu pour nous tester un peu !
+# Il a constaté que vous avez fait pas mal d'exercice en Python.
+# Ainsi, il vous demande de lui donner la clef de controle de sa carte de sécurité sociale
+# la clef de controle = 97 - (numero de sécurité sociale modulo 97) (le numero de securité sociale est de 13 chiffres)
+# exemple : pour le code sécurité sociale 1921071123456 la clef est de 15.
+def key_control_health(securite_social_num : int):
+    # TODO
+
+
+
